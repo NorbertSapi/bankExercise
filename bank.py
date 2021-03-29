6c77c66f7392bb5359c548c1fe0fd20c867e3811
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-03-29 14:47:18 +0100

    # This is a Bank class. # The class contains #   Properties of the class: first_name, second_name, account_number, sort_code, bank_card, balance #   Methods: get_user_name(), get_balance(), transfer()

--- bank.py
+++ bank.py
@@ -1,5 +1,33 @@
 # This is the bank account.
-# Variables:
+# Properties:
 #   username, account number(s), bank card(s), balance, credit
 # Methods:
 #   login, deposit, withdraw, transfer, block_bank_card, check_balance
+
+
+class Bank:
+    pass
+
+    print("This is the", __name__, "class.")
+
+    def __init__(self, first_name, second_name, account_number, sort_code, bank_card, balance):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.account_number = account_number
+        self.sort_code = sort_code
+        self.bank_card = bank_card
+        self.balance = balance
+
+# get user_name method
+    def get_user_name(self):
+        return self.first_name + " " + self.second_name
+
+# get balance method
+    def get_balance(self):
+        print("The balance of the", self.account_number, "account is", self.balance)
+
+# transfer money method
+    def transfer(self, number):
+        pass
+        if self.balance < number:
+            print("You do not have enough money to transfer, please deposit some money.")
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # This is a exercise to practice Objects. The exercise simulates a bank application.
+from bank import Bank
+
+# initialize the object
+user1 = Bank("John", "Smith", 12345678, 654321, 321456789, 0)
+
+user_name1 = Bank.get_user_name(user1)
+print(user_name1)
+Bank.get_balance(user1)
